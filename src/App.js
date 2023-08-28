add93464e75c229b20f64cdfe725f400ef43f790
Marco <cwhong8087@gmail.com>
2023-08-28 23:06:25 +0900

    2nd update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from "./logo.svg";
 import React from "react";
 import "./App.css";
 import { Component, Suspense, useState } from "react";
+import loadable from "@loadable/component";
 const SplitMe = React.lazy(() => import("./SplitMe"));
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
   };
 
   // class App extends Component {
@@ -27,7 +31,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hellooo React!</p>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hellooo React!
+        </p>
         <Suspense fallback={<div>loading...</div>}>
           {visible && <SplitMe />}
         </Suspense>
